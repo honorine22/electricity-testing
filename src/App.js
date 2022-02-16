5db92bd29207e4df8dabc435bac23a84a34f7ec8
honorine22 <higiranez@gmail.com>
2022-02-16 04:00:00 +0200

    Form Validation Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import Layout from './components/Layout'
 import Form from './components/Form'
-import electricit from './images/electricit.jpg';
 function App() {
   return (
     <Layout>
-      <img src={electricit} width="100" alt="Logo" />
       <Form />
     </Layout>
   );
